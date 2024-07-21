f5fe6815cefd976be706a0fb7925207a15e3bbf0
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-07-21 21:35:23 +0300

    upload files

--- src/Pages/Home/HeaderContainer.jsx
+++ src/Pages/Home/HeaderContainer.jsx
@@ -63,6 +63,129 @@ export default function HeaderContainer() {
                                     عربة التسوق الخاصة بك
                                 </Link>
                             </li>
+                            <li className='hidden-in-lg'>
+                                <Link to="/about">
+                                    من نحن
+                                </Link>
+                            </li>
+                            <li className='hidden-in-lg'>
+                                <Link to="/policies">
+                                    الشروط والاحكام
+                                </Link>
+                            </li>
+                            <li className='hidden-in-lg'>
+                                <Link to="/about">
+                                    قائمة الرغبات
+                                </Link>
+                            </li>
+                            <li className='hidden-in-lg'>
+                                <div className="user-profile dropdown">
+                                    <div className="user-cont dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false" >
+                                        <div className="user-info">
+                                            <span>الإنجليزية</span>
+                                            <i className="iconsax" icon-name="chevron-down"></i>
+                                        </div>
+                                    </div>
+                                    <ul className="profile-menu dropdown-menu">
+                                        <li>
+                                            <a href="/account/general">
+                                                <i className="iconsax" icon-name="user-1"></i>
+                                                <span>حسابي</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/orders/01">
+                                                <i className="iconsax" icon-name="basket-tick-1"></i>
+                                                <span>طلباتي السابقة</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/#contact">
+                                                <i className="iconsax" icon-name="message-dash-2"></i>
+                                                <span>تواصل معنا</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/login" className="logout">
+                                                <i className="iconsax" icon-name="logout-2"></i>
+                                                <span>الخروج</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li className='hidden-in-lg'>
+                                <div className="user-profile dropdown">
+                                    <div className="user-cont dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false" >
+                                        <div className="user-info">
+                                            <span>الدولار الأمريكي</span>
+                                            <i className="iconsax" icon-name="chevron-down"></i>
+                                        </div>
+                                    </div>
+                                    <ul className="profile-menu dropdown-menu">
+                                        <li>
+                                            <a href="/account/general">
+                                                <i className="iconsax" icon-name="user-1"></i>
+                                                <span>حسابي</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/orders/01">
+                                                <i className="iconsax" icon-name="basket-tick-1"></i>
+                                                <span>طلباتي السابقة</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/#contact">
+                                                <i className="iconsax" icon-name="message-dash-2"></i>
+                                                <span>تواصل معنا</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/login" className="logout">
+                                                <i className="iconsax" icon-name="logout-2"></i>
+                                                <span>الخروج</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li className='hidden-in-lg'>
+                                <div className="user-profile dropdown">
+                                    <div className="user-cont dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false" >
+                                        <div className="user-info">
+                                            <span>تتبع الطلب</span>
+                                            <i className="iconsax" icon-name="chevron-down"></i>
+                                        </div>
+                                    </div>
+                                    <ul className="profile-menu dropdown-menu">
+                                        <li>
+                                            <a href="/account/general">
+                                                <i className="iconsax" icon-name="user-1"></i>
+                                                <span>حسابي</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/orders/01">
+                                                <i className="iconsax" icon-name="basket-tick-1"></i>
+                                                <span>طلباتي السابقة</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/#contact">
+                                                <i className="iconsax" icon-name="message-dash-2"></i>
+                                                <span>تواصل معنا</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="/login" className="logout">
+                                                <i className="iconsax" icon-name="logout-2"></i>
+                                                <span>الخروج</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                     <div className="header-btn">
